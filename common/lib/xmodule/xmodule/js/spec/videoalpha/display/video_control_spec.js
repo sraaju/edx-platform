47ef83e44490e1801d9cd9ade8f154e385186a09
jmclaus <jmc@edx.org>
2013-07-11 14:01:09 -0400

    All tests pass in video_control_spec.js

--- common/lib/xmodule/xmodule/js/spec/videoalpha/display/video_control_spec.js
+++ common/lib/xmodule/xmodule/js/spec/videoalpha/display/video_control_spec.js
@@ -1,90 +1,83 @@
 (function() {
   describe('VideoControlAlpha', function() {
-    beforeEach(function() {
-      window.onTouchBasedDevice = jasmine.createSpy('onTouchBasedDevice').andReturn(false);
-      loadFixtures('videoalpha.html');
-      $('.video-controls').html('');
-    });
+    var state, videoControl;
+
+    function initialize() {
+      loadFixtures('videoalpha_all.html');
+      state = new VideoAlpha('#example');
+      videoControl = state.videoControl;
+    }
 
     describe('constructor', function() {
+      beforeEach(function() {
+        window.onTouchBasedDevice = jasmine.createSpy('onTouchBasedDevice').andReturn(false);
+        initialize();
+      });
+
       it('render the video controls', function() {
-        this.control = new window.VideoControlAlpha({
-          el: $('.video-controls')
-        });
-        expect($('.video-controls')).toContain;
-        ['.slider', 'ul.vcr', 'a.play', '.vidtime', '.add-fullscreen'].join(',');
+        expect($('.video-controls')).toContain(
+          ['.slider', 'ul.vcr', 'a.play', '.vidtime', '.add-fullscreen'].join(',')
+        ); //Should we add '.quality_control' and '.hide-subtitles'?
         expect($('.video-controls').find('.vidtime')).toHaveText('0:00 / 0:00');
       });
 
       it('bind the playback button', function() {
-        this.control = new window.VideoControlAlpha({
-          el: $('.video-controls')
+        expect($('.video_control')).toHandleWith('click', videoControl.togglePlayback);
+      });
+
+      describe('when on a non-touch based device', function() {
+        beforeEach(function() {
+          initialize();
         });
-        expect($('.video_control')).toHandleWith('click', this.control.togglePlayback);
+
+        it('add the play class to video control', function() {
+          expect($('.video_control')).toHaveClass('play');
+          expect($('.video_control')).toHaveAttr('title', 'Play');
+        });
       });
 
       describe('when on a touch based device', function() {
         beforeEach(function() {
           window.onTouchBasedDevice.andReturn(true);
-          this.control = new window.VideoControlAlpha({
-            el: $('.video-controls')
-          });
+          initialize();
         });
 
         it('does not add the play class to video control', function() {
           expect($('.video_control')).not.toHaveClass('play');
-          expect($('.video_control')).not.toHaveHtml('Play');
-        });
-      });
-      
-      describe('when on a non-touch based device', function() {
-        beforeEach(function() {
-          this.control = new window.VideoControlAlpha({
-            el: $('.video-controls')
-          });
-        });
-
-        it('add the play class to video control', function() {
-          expect($('.video_control')).toHaveClass('play');
-          expect($('.video_control')).toHaveHtml('Play');
+          expect($('.video_control')).not.toHaveAttr('title', 'Play');
         });
       });
     });
 
     describe('play', function() {
       beforeEach(function() {
-        this.control = new window.VideoControlAlpha({
-          el: $('.video-controls')
-        });
-        this.control.play();
+        initialize();
+        videoControl.play();
       });
 
       it('switch playback button to play state', function() {
         expect($('.video_control')).not.toHaveClass('play');
         expect($('.video_control')).toHaveClass('pause');
-        expect($('.video_control')).toHaveHtml('Pause');
+        expect($('.video_control')).toHaveAttr('title', 'Pause');
       });
     });
+
     describe('pause', function() {
       beforeEach(function() {
-        this.control = new window.VideoControlAlpha({
-          el: $('.video-controls')
-        });
-        this.control.pause();
+        initialize();
+        videoControl.pause();
       });
 
       it('switch playback button to pause state', function() {
         expect($('.video_control')).not.toHaveClass('pause');
         expect($('.video_control')).toHaveClass('play');
-        expect($('.video_control')).toHaveHtml('Play');
+        expect($('.video_control')).toHaveAttr('title', 'Play');
       });
     });
 
     describe('togglePlayback', function() {
       beforeEach(function() {
-        this.control = new window.VideoControlAlpha({
-          el: $('.video-controls')
-        });
+        initialize();
       });
 
       describe('when the control does not have play or pause class', function() {
@@ -95,48 +88,24 @@
         describe('when the video is playing', function() {
           beforeEach(function() {
             $('.video_control').addClass('play');
-            spyOnEvent(this.control, 'pause');
-            this.control.togglePlayback(jQuery.Event('click'));
+            spyOnEvent(videoControl, 'pause');
+            videoControl.togglePlayback(jQuery.Event('click'));
           });
 
           it('does not trigger the pause event', function() {
-            expect('pause').not.toHaveBeenTriggeredOn(this.control);
+            expect('pause').not.toHaveBeenTriggeredOn(videoControl);
           });
         });
 
         describe('when the video is paused', function() {
           beforeEach(function() {
             $('.video_control').addClass('pause');
-            spyOnEvent(this.control, 'play');
-            this.control.togglePlayback(jQuery.Event('click'));
+            spyOnEvent(videoControl, 'play');
+            videoControl.togglePlayback(jQuery.Event('click'));
           });
 
           it('does not trigger the play event', function() {
-            expect('play').not.toHaveBeenTriggeredOn(this.control);
-          });
-        });
-
-        describe('when the video is playing', function() {
-          beforeEach(function() {
-            spyOnEvent(this.control, 'pause');
-            $('.video_control').addClass('pause');
-            this.control.togglePlayback(jQuery.Event('click'));
-          });
-
-          it('trigger the pause event', function() {
-            expect('pause').toHaveBeenTriggeredOn(this.control);
-          });
-        });
-
-        describe('when the video is paused', function() {
-          beforeEach(function() {
-            spyOnEvent(this.control, 'play');
-            $('.video_control').addClass('play');
-            this.control.togglePlayback(jQuery.Event('click'));
-          });
-          
-          it('trigger the play event', function() {
-            expect('play').toHaveBeenTriggeredOn(this.control);
+            expect('play').not.toHaveBeenTriggeredOn(videoControl);
           });
         });
       });
